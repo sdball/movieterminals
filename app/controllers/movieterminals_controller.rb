fe19bc819cb69a63da871a0bec7b98ab56164ea0
Stephen Ball <sdball@gmail.com>
2011-10-15 01:17:34 -0400

    added first draft of 'alien' movie terminal

--- app/controllers/movieterminals_controller.rb
+++ app/controllers/movieterminals_controller.rb
@@ -2,4 +2,10 @@ class MovieterminalsController < ApplicationController
   def home
   end
 
+  def alien
+    @movie_title = 'Alien'
+    @movie_script = @movie_style = 'alien'
+    render 'alien'
+  end
+
 end
