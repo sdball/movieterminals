968aae86f5426cf744aebdff9b72f4641386da33
Stephen Ball <sdball@gmail.com>
2011-10-14 20:36:23 -0400

    new rails 3.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movieterminals::Application.config.secret_token = 'cc5155bce663088c6c42bf4e514dd3bc475452a18f43caa854e8e48e9fead00e8e19458dd38973812c597fd06cc4bbb373f829d3284510b3be4e3ab0110e479d'
