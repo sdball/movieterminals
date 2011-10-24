dd7af69ca610b05ad53372e00dd3454e1677f84a
Stephen Ball <sdball@gmail.com>
2011-10-23 20:27:38 -0400

    moved test_track and jasmine back into development/test
    Turns out, you totally can restrict routes to
    specific rails environments. It helps to read the
    docs, all of them.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Movieterminals::Application.routes.draw do
   match '/alien' => 'movieterminals#alien', :as => 'alien'
   match '/alien/reference' => 'references#alien', :as => 'alien_reference'
   root :to => 'pages#home'
-  mount TestTrack::Engine => "test"
+  mount TestTrack::Engine => "test" unless Rails.env.production?
 end
