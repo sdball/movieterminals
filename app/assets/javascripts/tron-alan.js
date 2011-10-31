9186e8106b622cc7352f00a171488bf24daaa1c1
Stephen Ball <sdball@gmail.com>
2011-10-30 21:20:28 -0400

    finished date command
    correctly tracks elapsed time in-universe

--- app/assets/javascripts/tron-alan.js
+++ app/assets/javascripts/tron-alan.js
@@ -45,11 +45,41 @@ alanone.start_service = function(service) {
       alanone.terminal.print('SERVICE "' + service + '" NOT REGISTERED');
   }
 }
-// TODO: initialize date on script start to some baseline e.g. Wed Jul 07 11:12:43 EDT 2011
-//       initialize "now" in seconds
-// date function should count seconds from script start to calc "current" date
+
+// calculate date with elapsing time
+alanone.initial_epochtime = new Date().getTime();
+alanone.in_universe_epochtime = new Date(1982, 06, 07, 11, 12, 43).getTime();
 alanone.commands['date'] = function() {
-  alanone.terminal.print('Wed Jul 07 11:12:43 EDT 1982');
+  var elapsed_epochtime = new Date().getTime() - alanone.initial_epochtime;
+  var universe_date = new Date(alanone.in_universe_epochtime + elapsed_epochtime);
+  alanone.terminal.print(alanone.timestamp(universe_date));
+}
+
+// convert a date object to an EST timestamp
+// can't just use toLocaleString here since I want all users to get the same experience
+// Wed Jul 07 1982 11:12:43 GMT-0400 (EDT)
+alanone.timestamp = function(date) {
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var timestamp = [];
+  timestamp.push(days[date.getUTCDay()]);
+  timestamp.push(months[date.getUTCMonth()]);
+  timestamp.push(alanone.leading_zero(date.getUTCDate()));
+  timestamp.push(date.getUTCFullYear());
+  var hours = alanone.leading_zero(date.getUTCHours() - 4);
+  var minutes = alanone.leading_zero(date.getUTCMinutes());
+  var seconds = alanone.leading_zero(date.getUTCSeconds());
+  timestamp.push(hours + ':' + minutes + ':' + seconds);
+  timestamp.push('GMT-0400 (EDT)');
+  return timestamp.join(' ');
+}
+
+alanone.leading_zero = function(number) {
+  if (number <= 9) {
+    return ['0', number].join('');
+  } else {
+    return number;
+  }
 }
 
 $(document).ready(function() {
