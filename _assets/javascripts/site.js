e0589746943c79e974517c8f40d63171807c48de
Stephen Ball <45837+sdball@users.noreply.github.com>
2024-09-21 10:05:09 -0400

    Switch entirely over to a fly.io static site

--- _assets/javascripts/site.js
+++
@@ -1,1 +0,0 @@
-//= require vendor/jquery.min-1.4.2.js
--- _assets/javascripts/vendor/prototypal_inheritance.js
+++
@@ -1,9 +0,0 @@
-// http://javascript.crockford.com/prototypal.html
-if (typeof Object.create !== 'function') {
-    Object.create = function (o) {
-        function F() {}
-        F.prototype = o;
-        return new F();
-    };
-}
-// newObject = Object.create(oldObject);
